c1581118b28cc234eb894de8ca1baecc08dbb0f8
Sam Bisbee <sam@sbisbee.com>
2011-11-02 13:43:19 -0400

    code styling

--- build/jshint-check.js
+++ build/jshint-check.js
@@ -1,21 +1,17 @@
-var JSHINT = require("./lib/jshint/jshint.js").JSHINT,
-  print = require("sys").print,
-  src = require("fs").readFileSync(process.argv[2], "utf8");
+var JSHINT = require("./lib/jshint/jshint.js").JSHINT;
+var print = require("sys").print;
+var src = require("fs").readFileSync(process.argv[2], "utf8");
 
-JSHINT(
-  src,
-  {
-    evil: true,
-    forin: true,
-    maxerr: 100,
-    noempty: true,
-    nomen: true,
-    node: true
-  }
-);
+JSHINT(src, {
+  evil: true,
+  forin: true,
+  maxerr: 100,
+  noempty: true,
+  nomen: true,
+  node: true
+});
 
 var ok = {
-
   // w.reason
   "Expected an identifier and instead saw 'undefined' (a reserved word).": true,
   "Use '===' to compare with 'null'.": true,
@@ -28,25 +24,25 @@ var ok = {
   "Expected an identifier and instead saw \'{a}\' (a reserved word).": true
 };
 
-var e = JSHINT.errors, 
-    found = 0, 
-    w;
+var e = JSHINT.errors;
+var found = 0;
+var w;
 
-for ( var i = 0; i < e.length; i++ ) {
+for(var i = 0; i < e.length; i++) {
   w = e[i];
 
   //console.log( w );
-  if ( !ok[ w.reason ] && !ok[ w.raw ] ) {
+  if(!ok[ w.reason ] && !ok[ w.raw ]) {
     found++;
-    print( "\n " + found + ". Problem at line " + w.line + " character " + w.character + ": " + w.reason );
-    print( "\n" + w.evidence );
+    print("\n " + found + ". Problem at line " + w.line + " character " + w.character + ": " + w.reason);
+    print("\n" + w.evidence);
   }
 }
 
-if ( found > 0 ) {
+if(found > 0) {
   print( "\n\n" + found + " Error(s) found.\n" );
   process.exit(1);
-
-} else {
-  print( "JSHint check passed.\n" );
+}
+else {
+  print("JSHint check passed.\n");
 }
