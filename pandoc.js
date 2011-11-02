4f71d93d6a6ab79a023d5f01451f98250163b9f9
Sam Bisbee <sam@sbisbee.com>
2011-11-02 13:16:01 -0400

    license info, first swipe at a readme, and, most importantly, the code from last night.

---
+++ pandoc.js
@@ -0,0 +1,58 @@
+var spawn = require('child_process').spawn;
+
+/*
+ * type is the input markup's type
+ *
+ * input is the input markup
+ *
+ * types is an array of markup types to convert to
+ *
+ * callback will be passed an object mapping markup type to markup, unless
+ * there was an error in which case it gets passed (null, statusCode)
+ */
+exports.convert = function(type, input, types, callback) {
+  console.log(type, input, types, callback);
+
+  var res = {};
+  res[type] = input;
+
+  var targetResponses = types.length - 1;
+  var numResponses = 0;
+
+  for(var i in types) {
+    /*
+     * This if-block filters out the target markup type because we already set
+     * it on the res object.
+     */
+    if(!res[types[i]]) {
+      var pandoc = spawn('pandoc', [ '-f', type, '-t', types[i] ]);
+
+      //so that we have the target type in scope on('data') - love ya some asynch
+      pandoc.stdout.targetType = types[i];
+
+      pandoc.stdout.proc = pandoc;
+
+      pandoc.stdout.on('data', function(data) {
+        //data will be a binary stream if you don't cast it to a string
+        res[this.targetType] = data + '';
+      });
+
+      pandoc.on('exit', function(code, signal) {
+        console.log('pandoc just exited with code ' + code + ' and signal ' + signal);
+
+        numResponses++;
+
+        if(code !== 0) {
+          callback(null, code);
+        }
+        else if(numResponses === targetResponses) {
+          callback(res);
+        }
+      });
+
+      //pipe them the input
+      pandoc.stdin.write(input, 'utf8');
+      pandoc.stdin.end();
+    }
+  }
+};
